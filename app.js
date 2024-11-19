65328e7ea2535f817ad80ed04370e25de6e06339
abdulhayeekhan <abdulhayeekhan2@gmail.com>
2024-11-19 16:42:39 +0500

    update router

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ const port = process.env.PORT || 3000;
 
 //routes
 const countryRoutes = require('./routers/countries');
-const companyRoutes = require('./routers/company');
-const stateRouter = require('./routers/state');
-const userRouter = require('./routers/account');
-const upsRouter = require('./routers/ups')
-const trackingRouter = require('./routers/ups')
-const shipmentRouter = require('./routers/shipment')
+// const companyRoutes = require('./routers/company');
+// const stateRouter = require('./routers/state');
+// const userRouter = require('./routers/account');
+// const upsRouter = require('./routers/ups')
+// const trackingRouter = require('./routers/ups')
+// const shipmentRouter = require('./routers/shipment')
 // const router = require('./routers/cities');
 app.use(cors());
 
@@ -40,11 +40,11 @@ app.use("/api",(req,res) =>{
 
 
 app.use('/country', countryRoutes);
-app.use('/company', companyRoutes);
-app.use('/state', stateRouter);
-app.use('/account', userRouter);
-app.use('/ups', upsRouter);
-app.use('/shipment', shipmentRouter);
+// app.use('/company', companyRoutes);
+// app.use('/state', stateRouter);
+// app.use('/account', userRouter);
+// app.use('/ups', upsRouter);
+// app.use('/shipment', shipmentRouter);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
